319465c85a2c3348b6dcab9d79d1f8d130106a40
LJW0401 <114296633+LJW0401@users.noreply.github.com>
2023-04-09 10:05:39 +0800

    Update Grayscale_Image.py

--- Grayscale_Image/Grayscale_Image.py
+++ Grayscale_Image/Grayscale_Image.py
@@ -9,62 +9,80 @@ from PIL import Image, ImageTk
 
 CANVAS_WIDTH=800
 CANVAS_HEIGHT=800
-'''basic operation'''
-def App_path():#get the path of the main program
+'''基本操作'''
+def App_path():#获得主程序的路径
     import sys
     """Returns the base application path."""
     if hasattr(sys, 'frozen'):
-        return os.path.dirname(sys.executable)
-    return os.path.dirname(__file__)
+        return os.path.dirname(sys.executable)  #使用pyinstaller打包后的exe目录
+    return os.path.dirname(__file__)            #没打包前的py目录
 
 
-def mkdir(path):#build the folder path
+def mkdir(path):#创建文件夹目录
     isExists = os.path.exists(path)
     if not isExists:
+        # 如果不存在则创建目录
+        # 创建目录操作函数
         os.makedirs(path)
 
 
-def refine_FileName(Path:str):#return proper file path
+def refine_FileName(Path:str):#返回合适格式的文件路径
     return '/'.join(Path.split('\\'))
 
-def get_file_name(Path:str):#return file name
+def get_file_name(Path:str):#返回文件名
     return Path.split('\\')[-1]
 
-'''relevant function'''
-#load image
+'''相关函数'''
+#加载图像
 def load_picture(path):
     img = cv2.imread(path,cv2.IMREAD_GRAYSCALE)
     return img
 
 
-#get thresh
+#获取阈值
 def get_thresh(img):
     return round(img.mean())
 
 
-#deal image
+#处理图像
 def deal_img(img,mid=128):
-    ret,dst=cv2.threshold(img,mid,255,cv2.THRESH_BINARY)
-    return dst
+    # Apply adaptive thresholding
+    threshed = cv2.adaptiveThreshold(img, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 11, 2)
+    img_filtered=threshed.copy()
+
+    for _ in range(10):
+        # Apply median filter to remove salt-and-pepper noise
+        img_filtered = cv2.medianBlur(img_filtered, 3)
+        # Apply bilateral filter to preserve edges and details
+        img_filtered = cv2.bilateralFilter(img_filtered, 7, 75, 75)
+    # cv2.imshow('Thresholded Image', cv2.resize(np.hstack((threshed,img_filtered)),(0,0),fx=0.25,fy=0.25))
+    # cv2.waitKey(0)
+    # cv2.destroyAllWindows()
+    return img_filtered
 
 
-'''relevant class'''
+'''相关的类'''
 
+
+
+'''定义软件的类'''
 class Grayfy_Image:
     def __init__(self) -> None:
-        """init"""
-        self.version='1.0.0'
+        """初始化类"""
+        self.version='2.0.0'
         self.OriImg=None
+        self.GrayImg=None
         self.Thresh=128
         self.ShowImg=None
         self.ZoomRatio=1
         self.ImgWidth=0
         self.ImgHeight=0
+        
     
 
-#init function
+#初始化函数
     def App_init(self):
-        """init app"""
+        """程序初始化"""
         self.root=tk.Tk()
         self.root.title(f"图像黑白化 v{self.version}")
         self.root.geometry('800x900')
@@ -72,10 +90,11 @@ class Grayfy_Image:
 
 
     def Assembly_init(self):
-        """init assembly"""
-    #the part to show image
+        """控件初始化"""
+    #显示图像的部分
         Frame_ShowImage=tk.Frame(
             self.root,
+            # relief='groove',bd=1
             )
         Frame_ShowImage.pack(side='top')
 
@@ -87,15 +106,17 @@ class Grayfy_Image:
         )
         self.Canvas_Image.pack(side='top')
 
-    #the part to handle
+    #操作按键区域
         Frame_Handle=tk.Frame(
             self.root,
+            # relief='groove',bd=1
             )
         Frame_Handle.pack(side='top',fill='x')
 
-        #the image path
+        #图片地址区域
         Frame_ImagePath=tk.Frame(
             Frame_Handle,
+            # relief='groove',bd=1
             )
         Frame_ImagePath.pack(side='top',fill='x')
 
@@ -105,7 +126,7 @@ class Grayfy_Image:
             font=('黑体',15)
             ).pack(side='left')
         
-        self.ImagePath=tk.StringVar(value='---init path---')
+        self.ImagePath=tk.StringVar(value='E:\homework_img_deal\original\w1-1.jpg')
         self.Entry_ImagePath=tk.Entry(
             Frame_ImagePath,
             textvariable=self.ImagePath,
@@ -115,9 +136,10 @@ class Grayfy_Image:
 
 
 
-        #Grayscale_Image button
+        #图像黑白化的按钮
         Frame_Button_Handling=tk.Frame(
             Frame_Handle,
+            # relief='groove',bd=1
         )
         Frame_Button_Handling.pack(side='top',fill='x')
         Button_Handling=tk.Button(
@@ -128,12 +150,23 @@ class Grayfy_Image:
         )
         Button_Handling.pack(side='left',pady=5,padx=5)
 
-        #show thresh
+        #保存图像
+        self.Button_Save_Image=tk.Button(
+            Frame_Button_Handling,
+            text='保存图像',
+            font=('黑体',15),
+            command=self.Button_Save_Image_Func
+        )
+        self.Button_Save_Image.pack(side='right',padx=5)
+
+        #黑白化时的阈值
         Frame_Mid_Thresh=tk.Frame(
             Frame_Button_Handling,
+            # relief='groove',bd=1
         )
-        Frame_Mid_Thresh.pack(side='left',fill='x',padx=100)
+        # Frame_Mid_Thresh.pack(side='left',fill='x',padx=100)
 
+        
         self.Button_Mid_Thresh_DD=tk.Button(
             Frame_Mid_Thresh,
             text='<<',
@@ -165,16 +198,21 @@ class Grayfy_Image:
             command=self.Button_Mid_Thresh_UU_Func
         )
         self.Button_Mid_Thresh_UU.pack(side='left',padx=5)
-
+        # tk.Label(Frame_Mid_Thresh,width=30).pack(side='right')
+        # Scroll_Mid_Thresh=tk.Scrollbar(
+        #     Frame_Mid_Thresh,
+        #     orient='horizontal'
+        # )
+        # Scroll_Mid_Thresh.pack(side='left',padx=5,fill='x',expand=True)
         
-#functions for assemblies
+#控件函数
     def Blank_Func(self):
         return
 
     def Button_Handling_Func(self):
         self.OriImg = load_picture(self.ImagePath.get())
         print('Load Sussessfully')
-        self.Thresh = get_thresh(self.OriImg)
+        self.Thresh = 120 #get_thresh(self.OriImg)
         self.Grayfy_Image()
         
         self.Label_Mid_Thresh['text']=str(self.Thresh)
@@ -225,26 +263,39 @@ class Grayfy_Image:
         self.Label_Mid_Thresh['text']=str(self.Thresh)
 
 
-#function
-    def Grayfy_Image(self):   #grayscale image
-        Img_Done=deal_img(self.OriImg,self.Thresh)
+    def Button_Save_Image_Func(self):
         path_=App_path()+'/'+get_file_name(self.ImagePath.get())
-        cv2.imwrite(path_,Img_Done)  #save image
+        cv2.imwrite(path_,self.GrayImg)#保存图像
+        
+#功能函数
+    def Grayfy_Image(self):#灰度化图像
+        # Img_Done=deal_img(self.OriImg,self.Thresh)
+        # path_=App_path()+'/'+get_file_name(self.ImagePath.get())
+        # cv2.imwrite(path_,Img_Done)#保存图像
+        self.GrayImg=deal_img(self.OriImg,self.Thresh)
+        
 
 
     def Show_Img(self):
         ResizeShape=(int(self.ImgWidth*self.ZoomRatio),int(self.ImgHeight*self.ZoomRatio))
-        img = Image.open(App_path()+'/'+get_file_name(self.ImagePath.get())).resize(ResizeShape)
+        # img = Image.open(App_path()+'/'+get_file_name(self.ImagePath.get())).resize(ResizeShape)
+        # self.ShowImg = ImageTk.PhotoImage(img)
+        img = Image.fromarray(self.GrayImg).resize(ResizeShape)
+        # img.show()
         self.ShowImg = ImageTk.PhotoImage(img)
+        # self.ShowImg.resize(ResizeShape)
         self.Canvas_Image.create_image(0,0,anchor='nw',image=self.ShowImg )
 
+        # cv2.imshow('Thresholded Image', cv2.resize(self.GrayImg,(0,0),fx=0.25,fy=0.25))
+        # cv2.waitKey(0)
+        # cv2.destroyAllWindows()
 
     def Run_app(self):
-        """run"""
+        """运行程序"""
         self.root.mainloop()
 
 
-'''run'''
+'''软件运行'''
 GrayfyImage=Grayfy_Image()
 GrayfyImage.App_init()
 GrayfyImage.Assembly_init()
