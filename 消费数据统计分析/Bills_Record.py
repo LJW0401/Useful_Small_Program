f925e97c2a530428e7c32a49a4ad4107e0901ae7
Jiwei Liu <114296633+LJW0401@users.noreply.github.com>
2023-05-17 09:00:03 +0800

    Update Bills_Record.py

--- 消费数据统计分析/Bills_Record.py
+++ 消费数据统计分析/Bills_Record.py
@@ -17,14 +17,20 @@ class Bills_Record:
     用于进行消费分析的集成类，集成了相关的函数以及变量\n
     注：返回的图片都是(np.ndarray类型的RGB图像)
     该类包含以下的分析功能：\n
+        Bills_Record.Check():用于检查类是否正常加载\n
         Bills_Record.Load_Bills_Excel():从电脑上加载Excel数据集(.xlsx文件)\n
         Bills_Record.Save_Bills_Excel():将内部数据保存为Excel数据集(.xlsx文件)\n
         Bills_Record.Add_New_Record():向表中添加新的消费记录\n
+        Bills_Record.Get_Types_And_Datas():获取指定时间段内的消费记录按不同类别划分的总类别及每个类别下的数据\n
+        Bills_Record.Draw_Bar_Chart():绘制柱状图\n
+        Bills_Record.Draw_Pie_Chart():绘制饼图\n
+        Bills_Record.Draw_Line_Chart():绘制折线图\n
         Bills_Record.Monthly_Consumption_Type_Analyse():分析指定月份的消费额与消费类型的关系，返回一张饼图和柱状图\n
         Bills_Record.Monthly_Consumption_Method_Analyse():分析指定月份的消费额与消费方式的关系，返回一张饼图和柱状图\n
         Bills_Record.Yearly_Consumption_Type_Analyse():分析指定年份的消费额与消费类型的关系，返回一张饼图和柱状图\n
         Bills_Record.Yearly_Consumption_Method_Analyse():分析指定年份的消费额与消费方式的关系，返回一张饼图和柱状图\n
         Bills_Record.Year_Monthly_Consumption_Change_Analyse():分析指定年份中每月消费总额的变化趋势，返回一张折线图\n
+        Bills_Record.Day_Average():计算指定时间段内的日均消费额\n
     '''
     def __init__(self) -> None:
         self.Bills=None
@@ -51,16 +57,14 @@ class Bills_Record:
         self.Bills = self.Bills.append(DataDict, ignore_index=True)
     
     
-    def Monthly_Consumption_Type_Analyse(self,year=2023,month=1):
-        '''分析指定月份的消费额与消费类型的关系，返回一张饼图和柱状图'''
-        start_date = pd.Timestamp(f'{year}-{month}-01')
-        end_date = start_date + pd.offsets.MonthEnd(1)
-        mask=(self.Bills['日期'] >= start_date) & (self.Bills['日期'] <= end_date)
+    def Get_Types_And_Datas(self,StartDate:pd.Timestamp,EndDate:pd.Timestamp,colname:str):
+        '''获取指定时间段内的消费记录按不同类别划分的总类别及每个类别下的数据'''
+        mask=(self.Bills['日期'] >= StartDate) & (self.Bills['日期'] <= EndDate)
         SubBills=self.Bills.loc[mask]
-        Types=list(SubBills['消费类型'].unique())
+        Types=list(SubBills[colname].unique())
         Datas=[]
         for Type in Types[:]:
-            mask=self.Bills['消费类型']==Type
+            mask=self.Bills[colname]==Type
             SubSubBills=SubBills.loc[mask]
             Sum=abs(SubSubBills['消费额'].sum())
             if Sum<0.01:
@@ -68,100 +72,9 @@ class Bills_Record:
             else:
                 Datas.append(Sum)
         Types,Datas=zip(*sorted(list(zip(Types,Datas)),reverse=True,key=lambda x:x[1]))
+        return Types,Datas
         
-        return (
-            self.Draw_Pie_Chart(datas=Datas,labels=Types,title=f'{year}-{month}消费分类百分比，消费总额:{round(sum(Datas),2)}'),
-            self.Draw_Bar_Chart(y=Datas,x=Types,xlabel='消费类型',ylabel='消费额',title=f'{year}-{month}消费分类柱状图，消费总额:{round(sum(Datas),2)}')
-        )
-    
-    
-    def Monthly_Consumption_Method_Analyse(self,year=2023,month=1):
-        '''分析指定月份的消费额与消费方式的关系，返回一张饼图和柱状图'''
-        start_date = pd.Timestamp(f'{year}-{month}-01')
-        end_date = start_date + pd.offsets.MonthEnd(1)
-        mask=(self.Bills['日期'] >= start_date) & (self.Bills['日期'] <= end_date)
-        SubBills=self.Bills.loc[mask]
-        Methods=list(SubBills['支付方式'].unique())
-        Datas=[]
-        for Method in Methods[:]:
-            mask=self.Bills['支付方式']==Method
-            SubSubBills=SubBills.loc[mask]
-            Sum=abs(SubSubBills['消费额'].sum())
-            if Sum<0.01:
-                Methods.remove(Method)
-            else:
-                Datas.append(Sum)
-        Methods,Datas=zip(*sorted(list(zip(Methods,Datas)),reverse=True,key=lambda x:x[1]))
-        
-        return (
-            self.Draw_Pie_Chart(datas=Datas,labels=Methods,title=f'{year}-{month}消费支付方式百分比，消费总额:{round(sum(Datas),2)}'),
-            self.Draw_Bar_Chart(y=Datas,x=Methods,xlabel='消费支付方式',ylabel='消费额',title=f'{year}-{month}消费支付方式柱状图，消费总额:{round(sum(Datas),2)}')
-        )
-    
-    
-    def Yearly_Consumption_Type_Analyse(self,year=2023):
-        '''分析指定年份的消费额与消费类型的关系，返回一张饼图和柱状图'''
-        start_date = pd.Timestamp(f'{year}-01-01')
-        end_date = start_date + pd.offsets.MonthEnd(12)
-        mask=(self.Bills['日期'] >= start_date) & (self.Bills['日期'] <= end_date)
-        SubBills=self.Bills.loc[mask]
-        Types=list(SubBills['消费类型'].unique())
-        Datas=[]
-        for Type in Types[:]:
-            mask=self.Bills['消费类型']==Type
-            SubSubBills=SubBills.loc[mask]
-            Sum=abs(SubSubBills['消费额'].sum())
-            if Sum<0.01:
-                Types.remove(Type)
-            else:
-                Datas.append(Sum)
-        Types,Datas=zip(*sorted(list(zip(Types,Datas)),reverse=True,key=lambda x:x[1]))
-        
-        return (
-            self.Draw_Pie_Chart(datas=Datas,labels=Types,title=f'{year}消费分类百分比，消费总额:{round(sum(Datas),2)}'),
-            self.Draw_Bar_Chart(y=Datas,x=Types,xlabel='消费类型',ylabel='消费额',title=f'{year}消费分类柱状图，消费总额:{round(sum(Datas),2)}')
-        )
-    
-    
-    def Yearly_Consumption_Method_Analyse(self,year=2023):
-        '''分析指定年份的消费额与消费方式的关系，返回一张饼图和柱状图'''
-        start_date = pd.Timestamp(f'{year}-01-01')
-        end_date = start_date + pd.offsets.MonthEnd(12)
-        mask=(self.Bills['日期'] >= start_date) & (self.Bills['日期'] <= end_date)
-        SubBills=self.Bills.loc[mask]
-        Methods=list(SubBills['支付方式'].unique())
-        Datas=[]
-        for Method in Methods[:]:
-            mask=self.Bills['支付方式']==Method
-            SubSubBills=SubBills.loc[mask]
-            Sum=abs(SubSubBills['消费额'].sum())
-            if Sum<0.01:
-                Methods.remove(Method)
-            else:
-                Datas.append(Sum)
-        Methods,Datas=zip(*sorted(list(zip(Methods,Datas)),reverse=True,key=lambda x:x[1]))
-        
-        return (
-            self.Draw_Pie_Chart(datas=Datas,labels=Methods,title=f'{year}消费支付方式百分比，消费总额:{round(sum(Datas),2)}'),
-            self.Draw_Bar_Chart(y=Datas,x=Methods,xlabel='消费支付方式',ylabel='消费额',title=f'{year}消费支付方式柱状图，消费总额:{round(sum(Datas),2)}')
-        )
-    
-    
-    def Year_Monthly_Consumption_Change_Analyse(self,year=2023):
-        '''分析指定年份中每月消费总额的变化趋势，返回一张折线图'''
-        Datas=[]
-        for month in range(1,13):#计算每一个月的消费总额
-            start_date = pd.Timestamp(f'{year}-{month}-01')
-            end_date = start_date + pd.offsets.MonthEnd(1)
-            mask=(self.Bills['日期'] >= start_date) & (self.Bills['日期'] <= end_date)
-            Year_Month_ConsumptionDataFrame = self.Bills.loc[mask]
-            Sum=abs(Year_Month_ConsumptionDataFrame['消费额'].sum())
-            Datas.append(Sum)
-        
-        return self.Draw_Line_Chart(x=list([str(i)+'月' for i in range(1,13)]),y=Datas,label='消费额',xlabel='月份',ylabel='消费额',title=f'{year}年消费随月份变化曲线')
-    
-    
-#绘制图像的函数
+    #绘制图像的函数
     def Draw_Bar_Chart(self,x,y,xlabel='',ylabel='',title=''):
         '''Draw bar chart'''
         fig, ax = plt.subplots(figsize=(6, 6))
@@ -225,8 +138,80 @@ class Bills_Record:
         
         if __name__ == '__main__':
             plt.show()
-            
         return Image
+    
+    
+    def Monthly_Consumption_Type_Analyse(self,year=2023,month=1):
+        '''分析指定月份的消费额与消费类型的关系，返回一张饼图和柱状图'''
+        start_date = pd.Timestamp(f'{year}-{month}-01')
+        end_date = start_date + pd.offsets.MonthEnd(1)
+        Types,Datas = self.Get_Types_And_Datas(StartDate=start_date,EndDate=end_date,colname='消费类型')
+        
+        return (
+            self.Draw_Pie_Chart(datas=Datas,labels=Types,title=f'{year}-{month}消费分类百分比，消费总额:{round(sum(Datas),2)}'),
+            self.Draw_Bar_Chart(y=Datas,x=Types,xlabel='消费类型',ylabel='消费额',title=f'{year}-{month}消费分类柱状图，消费总额:{round(sum(Datas),2)}')
+        )
+    
+    
+    def Monthly_Consumption_Method_Analyse(self,year=2023,month=1):
+        '''分析指定月份的消费额与消费方式的关系，返回一张饼图和柱状图'''
+        start_date = pd.Timestamp(f'{year}-{month}-01')
+        end_date = start_date + pd.offsets.MonthEnd(1)
+        Methods,Datas = self.Get_Types_And_Datas(StartDate=start_date,EndDate=end_date,colname='支付方式')
+        
+        return (
+            self.Draw_Pie_Chart(datas=Datas,labels=Methods,title=f'{year}-{month}消费支付方式百分比，消费总额:{round(sum(Datas),2)}'),
+            self.Draw_Bar_Chart(y=Datas,x=Methods,xlabel='消费支付方式',ylabel='消费额',title=f'{year}-{month}消费支付方式柱状图，消费总额:{round(sum(Datas),2)}')
+        )
+    
+    
+    def Yearly_Consumption_Type_Analyse(self,year=2023):
+        '''分析指定年份的消费额与消费类型的关系，返回一张饼图和柱状图'''
+        start_date = pd.Timestamp(f'{year}-01-01')
+        end_date = start_date + pd.offsets.MonthEnd(12)
+        Types,Datas = self.Get_Types_And_Datas(StartDate=start_date,EndDate=end_date,colname='消费类型')
+
+        return (
+            self.Draw_Pie_Chart(datas=Datas,labels=Types,title=f'{year}消费分类百分比，消费总额:{round(sum(Datas),2)}'),
+            self.Draw_Bar_Chart(y=Datas,x=Types,xlabel='消费类型',ylabel='消费额',title=f'{year}消费分类柱状图，消费总额:{round(sum(Datas),2)}')
+        )
+    
+    
+    def Yearly_Consumption_Method_Analyse(self,year=2023):
+        '''分析指定年份的消费额与消费方式的关系，返回一张饼图和柱状图'''
+        start_date = pd.Timestamp(f'{year}-01-01')
+        end_date = start_date + pd.offsets.MonthEnd(12)
+        Methods,Datas = self.Get_Types_And_Datas(StartDate=start_date,EndDate=end_date,colname='支付方式')
+        
+        return (
+            self.Draw_Pie_Chart(datas=Datas,labels=Methods,title=f'{year}消费支付方式百分比，消费总额:{round(sum(Datas),2)}'),
+            self.Draw_Bar_Chart(y=Datas,x=Methods,xlabel='消费支付方式',ylabel='消费额',title=f'{year}消费支付方式柱状图，消费总额:{round(sum(Datas),2)}')
+        )
+    
+    
+    def Year_Monthly_Consumption_Change_Analyse(self,year=2023):
+        '''分析指定年份中每月消费总额的变化趋势，返回一张折线图'''
+        Datas=[]
+        for month in range(1,13):#计算每一个月的消费总额
+            start_date = pd.Timestamp(f'{year}-{month}-01')
+            end_date = start_date + pd.offsets.MonthEnd(1)
+            mask=(self.Bills['日期'] >= start_date) & (self.Bills['日期'] <= end_date)
+            Year_Month_ConsumptionDataFrame = self.Bills.loc[mask]
+            Sum=abs(Year_Month_ConsumptionDataFrame['消费额'].sum())
+            Datas.append(Sum)
+        
+        return self.Draw_Line_Chart(x=list([str(i)+'月' for i in range(1,13)]),y=Datas,label='消费额',xlabel='月份',ylabel='消费额',title=f'{year}年消费随月份变化曲线')
+    
+    
+    def Day_Average(self,StartDate:pd.Timestamp,EndDate:pd.Timestamp):
+        '''计算指定时间段内的日均消费额'''
+        mask=(self.Bills['日期'] >= StartDate) & (self.Bills['日期'] <= EndDate)
+        SubBills=self.Bills.loc[mask]
+        Sum=abs(SubBills['消费额'].sum())
+        Days=(EndDate-StartDate).days
+        return round(Sum/Days,2)
+    
+
     
     
 if __name__ == '__main__':
@@ -236,8 +221,9 @@ if __name__ == '__main__':
     # BillsRecord.Add_New_Record({'日期':pd.Timestamp('2025-01-01'),'消费额':-1000,'支付方式':'微信钱包','消费类型':'测试'})
     # print(BillsRecord.Bills)
     
-    BillsRecord.Monthly_Consumption_Type_Analyse(month=4)
+    # BillsRecord.Monthly_Consumption_Type_Analyse(month=5)
     # BillsRecord.Yearly_Consumption_Type_Analyse(year=2023)
     # BillsRecord.Yearly_Consumption_Method_Analyse(year=2023)
 
-    BillsRecord.Year_Monthly_Consumption_Change_Analyse(year=2023)
+    # BillsRecord.Year_Monthly_Consumption_Change_Analyse(year=2023)
+    print(BillsRecord.Day_Average(StartDate=pd.Timestamp('2023-05-01'),EndDate=pd.Timestamp('2023-05-31')))
